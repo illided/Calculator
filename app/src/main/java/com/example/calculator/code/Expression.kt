db128d1aba17a68dc919d39f3ad9468f6996f5e1
illided <utkin.ilya.nickolaevich@gmail.com>
2020-10-15 19:20:34 +0300

    changes reversed

--- app/src/main/java/com/example/calculator/code/Expression.kt
+++
@@ -1,7 +0,0 @@
-package com.example.calculator.code
-
-class Expression(private val expression: String) {
-    val value: Double by lazy {
-        Algorithm(Parser.parseToTokens(expression)).evaluate()
-    }
-}
